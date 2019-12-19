26cba93db179b437bab970694993c5dbb02b637b
Roma <king_roma@ukr.net>
2019-12-19 17:49:12 +0200

    \2

--- js/hw.5.3.js
+++ js/hw.5.3.js
@@ -1,18 +1,19 @@
-const Storage = function(items = []) {
-  this.items = items;
-};
 
-Storage.prototype.getItems = function() {
-  return this.items;
-};
+class Storage {
+  constructor(items = []) {
+    this.items = items;
+  }
 
-Storage.prototype.addItem = function(item) {
-  this.items.push(item);
-};
-
-Storage.prototype.removeItem = function(item) {
-  this.items.splice(this.items.indexOf(item), 1);
-};
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+}
 
 const storage = new Storage([
   "Нанитоиды",
